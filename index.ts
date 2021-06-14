954ae016f3bf6214455e69a5414d838a3a180c03
wscnd <wellingson@gmail.com>
2021-06-14 15:30:43 -0300

    Connect to prisma and return data

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import { PrismaClient } from "@prisma/client";
 import express from "express";
 import morgan from "morgan";
 import os from "os";
@@ -7,14 +8,25 @@ const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.get("/", async (req, res) => {
-  console.log(req);
-  res.json({ hello: "hello" });
+const prisma = new PrismaClient();
+
+app.get("/posts", async (req, res) => {
+  const posts = await prisma.post.findMany();
+  console.dir(posts, { depth: Infinity });
+  res.json(posts);
+});
+
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany();
+  console.dir(users, { depth: Infinity });
+  res.json(users);
 });
 
 const localIp = os.networkInterfaces()["eth0"]?.[0]?.address;
 const port = 3000;
 
-app.listen(port, () => {
-  console.log(`listening on http://${localIp}:${port}`);
+app.listen(port, async () => {
+  await prisma.$connect().then(() => {
+    console.log(`listening on http://${localIp}:${port}`);
+  });
 });
